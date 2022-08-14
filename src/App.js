1e55b8198859b79fe49042a1956cde597d82ef97
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-08-14 16:35:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { useState } from 'react';
+import MoleContainer from './components/MoleContainer';
 
 function App() {
+  let [score, setScore] = useState(0);
+
+  function addToScore(addedPoint) {
+    setScore(score + addedPoint);
+  } 
   return (
     <div className="App">
-      <header className="App-header">
-      React a Mole
-      </header>
+      <h1>React a Mole</h1>
+      <h2>Score {score}</h2>
+      <div className='mole-mounds'>
+      <MoleContainer addToScore={addToScore}/>
+      </div>
     </div>
   );
 }
